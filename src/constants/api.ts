c9e4a95d3c185371178b4e7fb82563efef1f291c
Emin <emintayfur@icloud.com>
2022-08-05 15:14:54 +0300

    ``api.ts`` dosyasında gözden kaçan sorunlar çözüldü

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,10 +1,10 @@
 export const DEVELOPMENT_SERVER_URL = 'http://localhost:4000';
-export const PRODUCTION_SERVER_URL = 'https://emins-tasks.herokuapp.com/';
+export const PRODUCTION_SERVER_URL = 'https://emins-tasks.herokuapp.com';
 
 export const SERVER_URL =
     process.env.NODE_ENV === 'development'
         ? DEVELOPMENT_SERVER_URL
-        : process.env?.SERVER_URL || PRODUCTION_SERVER_URL;
+        : PRODUCTION_SERVER_URL;
 
 export const pathWithServerUrl = (endpoint: string) => {
     if (endpoint[0] === '/') endpoint = endpoint.slice(1);
